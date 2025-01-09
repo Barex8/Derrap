d7f3b856c3fccc7a0ccfe903a45dc1ed17571b99
gabifdezz <letsgabii@gmail.com>
2025-01-09 12:10:52 +0100

    asd

---
+++ Derrap/src/Orden.java
@@ -0,0 +1,13 @@
+
+public class Orden {
+
+	int id;
+	String fecha_entrada;
+	String descripcion;
+	String pieza_sustituida;
+	String modelo_coche;
+	String matricula;
+	String servicio;
+	String estado_asignacion;
+	String estado_reparacion;
+}
